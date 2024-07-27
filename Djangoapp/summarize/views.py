9929ba34cd28c8f67bfb89069f5dfcbaeb1d8a41
unknown <pranjalkishor5@gmail.com>
2024-07-27 22:30:08 +0530

    Comments_done

--- Djangoapp/summarize/views.py
+++ Djangoapp/summarize/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import MessageSerializer
 from .models import Message
 from .gene import get_npresponse,get_ppresponse
 
@@ -23,6 +24,12 @@ def comments_n(request):
 
     resp=get_npresponse(current_text)
     return Response({'response_n':current_text})
+
+@api_view(['GET'])
+def comments(request):
+    all_messages=Message.objects.all()
+    seria=MessageSerializer(all_messages,many=True)
+    return Response(seria.data)
     
 
 
