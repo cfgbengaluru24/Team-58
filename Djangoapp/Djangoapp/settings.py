73c887fa01903ca1c97015c52c1008a9a3003b8d
Goutami Sooda <soodagoutami@gmail.com>
2024-07-28 00:07:53 +0530

    add cors headers

--- Djangoapp/Djangoapp/settings.py
+++ Djangoapp/Djangoapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'summarize'
+    'summarize',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+
 ]
 
 ROOT_URLCONF = 'Djangoapp.urls'
@@ -122,3 +125,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
+
+CORS_ALLOW_METHODS = [
+    "POST",
+    "GET",
+    "OPTIONS",
+]
+
+CORS_ALLOW_HEADERS = [
+    "Content-Type",
+    "Authorization"
+]
--- Djangoapp/summarize/urls.py
+++ Djangoapp/summarize/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('commentsp',views.comments_p,name="comments_p"),
-    path('commentsn',views.comments_n,name="comments_n"),
-    path('comments',views.comments,name="comments")
+    path('commentsp/',views.comments_p,name="comments_p"),
+    path('commentsn/',views.comments_n,name="comments_n"),
+    path('comments/',views.comments,name="comments")
 ]
