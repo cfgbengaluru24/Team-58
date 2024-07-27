999e75375c40d30e894556d48e264aa214e196dc
Goutami Sooda <soodagoutami@gmail.com>
2024-07-28 04:35:52 +0530

    Remove node modules

--- Djangoapp/Proctor_test/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProctorTestConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Proctor_test'
--- Djangoapp/summarize/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Message(models.Model):
-    username=models.CharField(max_length=100)
-    feedback=models.TextField(max_length=10000)
-    timestamp=models.DateTimeField()
