08662c5a28d1e0a7febd34d0d8bfe944645a2c2f
Goutami Sooda <soodagoutami@gmail.com>
2024-07-28 08:36:36 +0530

    Modify Online proctored test

--- Djangoapp/Proctor_test/urls.py
+++ Djangoapp/Proctor_test/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('test',views.test,name="test"),
-    path('video_feed',views.video_feed,name="video_feed")
+    path('video_feed',views.video_feed,name="video_feed"),
+    path('check_malpractice', views.check_malpractice),
 ]
--- Djangoapp/Proctor_test/views.py
+++ Djangoapp/Proctor_test/views.py
@@ -1,65 +1,21 @@
-# from django.shortcuts import render, redirect
-# import cv2
-# from django.http import StreamingHttpResponse
 
-# face_cascade = cv2.CascadeClassifier(r"C:\Users\sooda\cfg\Team-58\Djangoapp\haarcascade_frontalface_default.xml")
-
-# def gen(camera):
-#     while True:
-#         ret, frame = camera.read()
-        
-#         if not ret:
-#             break
-
-#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-#         if len(faces) > 1:
-#             camera.release()
-#             return redirect('/')  # Stop the camera and redirect if multiple faces are detected
-
-#         # Draw bounding boxes around faces
-#         for (x, y, w, h) in faces:
-#             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-#         _, jpeg = cv2.imencode('.jpg', frame)
-#         frame = jpeg.tobytes()
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-# def video_feed(request):
-#     camera = cv2.VideoCapture(0)
-#     return StreamingHttpResponse(gen(camera), content_type='multipart/x-mixed-replace; boundary=frame')
-
-# def test(request):
-#     return render(request, 'test.html')
-
-# def no(request):
-#     return render(request, "no.html")
 
 from django.shortcuts import render, redirect
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, JsonResponse
 import cv2
 
+malpractice_detected = False
 face_cascade = cv2.CascadeClassifier(r"C:\Users\sooda\cfg\Team-58\Djangoapp\haarcascade_frontalface_default.xml")
 
 def gen(camera):
+    global malpractice_detected
     while True:
         ret, frame = camera.read()
         
         if not ret:
             break
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-        if len(faces) > 1:
-            camera.release()
-            return b''  # Stop the camera if multiple faces are detected
-
-        # Draw bounding boxes around faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
 
         _, jpeg = cv2.imencode('.jpg', frame)
         frame = jpeg.tobytes()
@@ -73,6 +29,6 @@ def video_feed(request):
 def test(request):
     return render(request, 'test.html')
 
-def no(request):
-    return render(request, "no.html")
-
+def check_malpractice(request):
+    global malpractice_detected
+    return JsonResponse({'malpractice': malpractice_detected})
